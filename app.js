3aa0a3bf94ce65d6a878d44c95f2898844e767ec
Sam Hopkins <SamHopkins@Sams-Air.fios-router.home>
2020-05-07 16:22:17 -0400

    added responsive capability

---
+++ app.js
@@ -0,0 +1,11 @@
+// Website this is based off of
+// https://jacekjeznach.com/
+
+// Font Awesome API Token CC61A529-B980-4E53-B6C1-0758E18C56C3
+
+console.log('connected!')
+
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    anchors: ['home', 'projects', 'work', 'education']
+})
