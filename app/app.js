f71953fb3b9a513eeea9b8196f5882f1d0493039
Sam Hopkins <SamHopkins@Sams-Air.fios-router.home>
2020-06-11 17:53:51 -0400

    first commit from desktop

---
+++ app/app.js
@@ -0,0 +1,9 @@
+// Website this is based off of
+// https://jacekjeznach.com/
+
+// Font Awesome API Token CC61A529-B980-4E53-B6C1-0758E18C56C3
+
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    anchors: ['home', 'projects', 'work', 'education']
+});
